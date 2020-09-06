f015b26b5a9050f45ddfd89cbe092daa7660b679
foak77 <emailtofred@gmail.com>
2020-09-06 14:10:31 -0400

    picture in picture

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// PROMPT THE USER TO SELECT  MEDIA STREAM, PASS IT TO THE VIDEO ELEMENT AND PLAY
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    console.log(mediaStream);
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("Something went wrong here at:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // DISABLE THE BUTTON
+  button.disable = true;
+  // START PICTURE IN PICTURE
+  await videoElement.requestPictureInPicture();
+  // RESET THE BUTTON
+  button.disable = false;
+});
+
+selectMediaStream();
